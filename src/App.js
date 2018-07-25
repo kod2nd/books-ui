993049e4341a9994ba3f71da9a0451937fbcee46
kod2nd <colinongengkiat@gmail.com>
2018-07-25 14:39:29 +0800

    added books fetch data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  async getBooks() {
+    const response = await fetch('http://localhost:3000/books')
+    const data = await response.json()
+    this.setState({
+      books: data
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.books.map(book => {
+          return <li key={book._id}>{book.title}</li>
+        })}
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.getBooks()
   }
 }
 
